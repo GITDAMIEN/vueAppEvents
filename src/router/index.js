8d6be02546fc64234964fb4ed32e7eccafda4e62
Damiano Bellamoli <damiano.bellamoli@hotmail.it>
2022-07-24 21:12:09 +0200

    fix to 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -23,8 +23,8 @@ const routes = [
   },
 ]
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+const router = new VueRouter({
+  mode: 'history',
   routes: [
     {path: '*', component: NotFoundComponent}
   ]
